ad469da302a18a4bfddbfacdae7148b9fe6e0f56
cemcoe <chemxcode@gmail.com>
2021-01-21 09:24:26 +0800

    feat: base Profile user info display

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,34 @@
 <template>
-  <p>profile</p>
+  <section v-if="loginState">用户登录时展示
+    {{user.name}}
+  </section>
+
+  <section v-else>用户未登录展示
+    <router-link to="/login">请登录</router-link>
+  </section>
 </template>
 
 <script>
+import { computed } from "vue";
+import { useStore, mapState } from "vuex";
 export default {
+  setup() {
+    const store = useStore();
+    const loginState = computed(() => {
+      return store.state.token ? true : false;
+    });
 
-}
+    const user = computed(() => {
+      return store.state.user
+    })
+
+    return {
+      loginState,
+      user,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
