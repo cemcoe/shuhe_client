e7c446001da9382fa19f3bccbf3b77271571fd84
cemcoe <chemxcode@gmail.com>
2021-01-21 17:01:38 +0800

    feat: Profile header UI

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ export default createStore({
   state: {
     token: localStorage.getItem('token'),
     user: JSON.parse(localStorage.getItem('user')),
+    imgBaseUrl: "https://jian.cemcoe.com/jianshu_api"
   },
   mutations: {
     set_token(state, token) {
@@ -11,6 +12,8 @@ export default createStore({
       localStorage.setItem("token", token);
     },
     set_user(state, user) {
+      // 拿到完整的图片地址
+      user.avatar = state.imgBaseUrl + user.avatar
       state.user = user;
       localStorage.setItem('user', JSON.stringify(user))
     }
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,52 @@
 <template>
-  <section v-if="loginState">用户登录时展示
-    {{user.name}}
+  <section v-if="loginState">
+    <div class="user">
+      <div class="profile" v-if="user._id">
+        <div class="left">
+          <router-link :to="'/u/' + user._id">
+            <img :src="user.avatar" alt="avatar" />
+          </router-link>
+        </div>
+
+        <div class="center">
+          <router-link :to="'/u/' + user._id">
+            <span class="user-name">{{ user.name }}</span>
+          </router-link>
+          <div class="follow">
+            <router-link :to="'/users/' + user._id + '/following'">
+              <span class="follower">关注 0</span>
+            </router-link>
+
+            <router-link :to="'/users/' + user._id + '/followers'">
+              <span class="follower">粉丝0</span>
+            </router-link>
+
+            <!-- <span class="fans">粉丝 233</span> -->
+          </div>
+        </div>
+        <div class="right">简书创作者</div>
+      </div>
+      <div v-else>
+        <router-link :to="'/login/'">登录</router-link>
+      </div>
+      <hr />
+
+      <div class="mine">
+        <ul>
+          <li>
+            <router-link to="/mypost">我的文章</router-link>
+          </li>
+
+          <li>我的贴子</li>
+          <li>赞和收藏</li>
+          <li>我的书架</li>
+        </ul>
+      </div>
+    </div>
   </section>
 
-  <section v-else>用户未登录展示
+  <section v-else>
+    用户未登录展示
     <router-link to="/login">请登录</router-link>
   </section>
 </template>
@@ -19,8 +62,8 @@ export default {
     });
 
     const user = computed(() => {
-      return store.state.user
-    })
+      return store.state.user;
+    });
 
     return {
       loginState,
@@ -30,5 +73,59 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.user {
+  width: 90%;
+  margin: 10px auto;
+  box-shadow: 0px 0px 10px rgb(223, 217, 217);
+  padding: 16px;
+  border-radius: 6px;
+}
+.profile {
+  display: flex;
+  padding-bottom: 10px;
+}
+.left {
+  width: 50px;
+}
+img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+
+.center {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 6px;
+  font-size: 12px;
+}
+
+.user-name {
+  font-size: 16px;
+  font-weight: 300;
+  font-size: 12px;
+}
+
+.follower {
+  padding-right: 10px;
+}
+
+.right {
+  width: 60px;
+  color: #f40;
+  font-size: 12px;
+}
+
+.mine {
+  padding-top: 10px;
+}
+.mine ul {
+  display: grid;
+  grid-template-rows: 60px;
+  grid-template-columns: repeat(4, 25%);
+  text-align: center;
+}
 </style>
