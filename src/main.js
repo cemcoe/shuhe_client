7df0123912afe68f7046fee58eabd8b85b949b55
cemcoe <chemxcode@gmail.com>
2021-01-22 11:09:01 +0800

    feat: Home gotoEditor button

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import { CemButton } from 'cem-ui'
+import { CemButton, CemIcon } from 'cem-ui'
 
 const app = createApp(App)
-app.use(CemButton)
+app.use(CemButton).use(CemIcon)
 
 app.use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,12 @@
     home
     <cem-button type="warning">button</cem-button>
     <post-list :postList="postList"></post-list>
+
+    <div class="write">
+      <router-link to="/editor">
+        <cem-icon name="write"></cem-icon
+      ></router-link>
+    </div>
   </div>
 </template>
 
@@ -34,3 +40,27 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.write {
+  position: fixed;
+  width: 40px;
+  height: 40px;
+  right: 50px;
+  bottom: 80px;
+  border-radius: 50%;
+  /* background-color: rgb(236, 221, 221); */
+  background-color: #fff;
+  line-height: 40px;
+  text-align: center;
+  padding: 10px;
+  /* box-shadow: 0 0 40px rgb(8, 8, 8); */
+  box-shadow: 4px 4px 6px rgb(228, 225, 225);
+}
+.write a {
+  width: 100%;
+  height: 100%;
+  display: block;
+  color: #fff;
+}
+</style>
