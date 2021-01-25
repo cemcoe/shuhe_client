165f038ed2d355c9060902b5141b43a1a36f1e4c
cemcoe <chemxcode@gmail.com>
2021-01-25 10:07:34 +0800

    feat: Following FollowingUser display

--- src/views/Following/Following.vue
+++ src/views/Following/Following.vue
@@ -1,13 +1,31 @@
 <template>
-  <p>Following</p>
+  <following-user
+    :followingUsers="followingUsers"
+    :imgBaseUrl="imgBaseUrl"
+  ></following-user>
 </template>
 
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import FollowingUser from "./FollowingUser";
 export default {
+  components: {
+    FollowingUser,
+  },
+  setup() {
+    const store = useStore();
 
-}
+    const followingUsers = computed(() => store.state.followingUsers);
+    const imgBaseUrl = computed(() => store.state.imgBaseUrl);
+
+    return {
+      followingUsers,
+      imgBaseUrl,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/views/Following/FollowingUser.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="follow">
+      <div class="following" v-for="user in followingUsers" :key="user._id">
+        <div class="item">
+          <div class="left">
+            <router-link :to="'/u/' + user._id">
+              <img :src="imgBaseUrl + user.avatar" alt="avatar" />
+            </router-link>
+          </div>
+
+          <div class="center">
+            <span>
+              <router-link :to="'/u/' + user._id">
+                {{ user.name }}
+              </router-link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    followingUsers: {
+      type: Object,
+    },
+    imgBaseUrl: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.follow {
+  display: flex;
+
+  overflow-y: hidden;
+}
+.item {
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  flex-direction: column;
+  padding: 6px;
+  border-bottom: 1px solid rgb(207, 198, 207);
+}
+.left {
+  flex: 0 0 70px;
+}
+
+img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  padding: 2px;
+  border: 2px solid rgb(223, 77, 77);
+}
+</style>
