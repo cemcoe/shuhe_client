54fe96925a148861c103f60ca3cfecc5b0088dba
cemcoe <chemxcode@gmail.com>
2021-01-25 10:29:53 +0800

    feat: following PostList display

--- src/views/Following/Following.vue
+++ src/views/Following/Following.vue
@@ -3,25 +3,39 @@
     :followingUsers="followingUsers"
     :imgBaseUrl="imgBaseUrl"
   ></following-user>
+  <post-list :postList="postList"></post-list>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, onMounted, reactive, toRefs } from "vue";
 import { useStore } from "vuex";
 import FollowingUser from "./FollowingUser";
+import { listfollowingUser } from "network/user";
+import PostList from "components/content/PostList/PostList.vue";
+
 export default {
   components: {
     FollowingUser,
+    PostList,
   },
   setup() {
     const store = useStore();
 
     const followingUsers = computed(() => store.state.followingUsers);
     const imgBaseUrl = computed(() => store.state.imgBaseUrl);
+    const state = reactive({
+      postList: [],
+    });
+
+    onMounted(async () => {
+      const res = await listfollowingUser(store.state.user._id, "posts");
+      state.postList = res.data.postList;
+    });
 
     return {
       followingUsers,
       imgBaseUrl,
+      ...toRefs(state),
     };
   },
 };
