5ef3cfb0d170eb3f0ff0a72c963c2459d427c889
cemcoe <chemxcode@gmail.com>
2021-01-28 08:16:21 +0800

    feat: display Menu

--- src/views/Post/PostHeader.vue
+++ src/views/Post/PostHeader.vue
@@ -1,19 +1,84 @@
 <template>
-  <cem-nav-bar title="文章" @click-left="$router.back()">
+  <cem-nav-bar
+    title="文章"
+    @click-left="$router.back()"
+    @click-right="changeMenuStatus"
+  >
     <template #left>
       <cem-icon name="back" />
     </template>
     <template #right>
       <cem-icon name="more" />
+      <div class="menu" v-show="isShowMenu">
+        <ul>
+          <li>删除</li>
+        </ul>
+      </div>
     </template>
   </cem-nav-bar>
+  <div class="overlay" v-show="isShowMenu" @click="changeMenuStatus"></div>
 </template>
 
 <script>
+import { ref } from "vue";
+
+const useMenu = () => {
+  const isShowMenu = ref(false);
+
+  const changeMenuStatus = () => {
+    isShowMenu.value = !isShowMenu.value;
+  };
+
+  return {
+    isShowMenu,
+    changeMenuStatus,
+  };
+};
+
 export default {
   name: "PostHeader",
+  setup() {
+    const { isShowMenu, changeMenuStatus } = useMenu();
+
+    return {
+      isShowMenu,
+      changeMenuStatus,
+    };
+  },
 };
 </script>
 
 <style scoped>
+.menu {
+  position: fixed;
+  right: 0;
+  top: 44px;
+  z-index: 3504;
+  min-width: 80px;
+}
+.menu ul {
+  padding: 0;
+  margin: 0;
+}
+.menu li {
+  line-height: 30px;
+  padding: 6px;
+  text-align: left;
+  list-style: none;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  background-color: #fff;
+  box-shadow: 4px 4px 6px rgb(228, 225, 225);
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 2999;
+  background-color: #000;
+  opacity: 0.8;
+}
 </style>
