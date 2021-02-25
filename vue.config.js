7a37525134f7e5fcaf0d580d97919e3ca04a6f85
cemcoe <chemxcode@gmail.com>
2021-02-25 10:42:25 +0800

    fix: build fail no sw.js

--- vue.config.js
+++ vue.config.js
@@ -16,14 +16,6 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
   }
 }
 
