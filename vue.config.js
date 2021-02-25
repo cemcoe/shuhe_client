fc0e57e1f0acc85830ea419479972007d972ef87
cemcoe <chemxcode@gmail.com>
2021-02-25 10:34:47 +0800

    feat: update site meta

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,20 @@ module.exports = {
       }
     },
   }, 
+  pwa: {
+    name: '书核',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 }
 
